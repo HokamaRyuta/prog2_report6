b53dd0d4d63ec951753204a39a546f343c9ee5c4
Ryuta Hokama <e245703@ie.u-ryukyu.ac.jp>
2025-01-07 18:07:23 +0900

    フィールドを作成して描画する

--- app/src/main/java/jp/ac/uryukyu/ie/e245703/game/GameManage.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245703/game/GameManage.java
@@ -1,5 +1,24 @@
 package jp.ac.uryukyu.ie.e245703.game;
 
 public class GameManage {
-    
+    // 定数
+    public static final int TILE_SIZE = 30; // 1マスのサイズ（ピクセル）
+    public static final int ROWS = 20; // フィールドの行数
+    public static final int COLUMNS = 10; // フィールドの列数
+    public static final int SPACE = 5; // フィールドの上の空間(描画はしない)
+    // フィールド
+    private int[][] field;
+
+    public GameManage(){
+        this.field = new int[ROWS + SPACE][COLUMNS];  // 空白のフィールドを生成
+    }
+
+    public int getBlock(int y, int x){
+        return field[y][x];
+    }
+
+    public void setField(int[][] testField){ // テスト用のsetterメソッド(後々消去予定)
+        this.field = testField;
+    }
+
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e245703/ui/FieldPanel.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245703/ui/FieldPanel.java
@@ -1,5 +1,62 @@
 package jp.ac.uryukyu.ie.e245703.ui;
 
-public class FieldPanel {
+import static jp.ac.uryukyu.ie.e245703.game.GameManage.*;
+import jp.ac.uryukyu.ie.e245703.game.*;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 
+public class FieldPanel extends JPanel{
+    private static final long serialVersionUID = 1L;
+    // レイアウト
+    BorderLayout layout = new BorderLayout();
+    // GameManageの参照
+    GameManage gm;
+
+    // コンストラクタ
+    public FieldPanel(){
+        gm = new GameManage();
+        this.setLayout(layout); // レイアウト設定
+        this.setBackground(Color.black); // 背景の色
+        setPreferredSize(new Dimension(COLUMNS * TILE_SIZE, ROWS * TILE_SIZE)); // ウィンドウサイズ
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // フィールドを描画
+        for(int y = 0; y < ROWS; y++){
+            for(int x = 0; x < COLUMNS; x++){
+                if(gm.getBlock(y + SPACE, x) > 0){
+                    // ブロックの描画
+                    g.setColor(getBlockColor(gm.getBlock(y + SPACE, x)));
+                    g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                    // 枠線の描画
+                    g.setColor(Color.gray);
+                    g.drawRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                }
+            }
+        }
+    }
+
+    // テトリミノの種類ごとに色を取得するメソッド
+    public Color getBlockColor(int shapeNumber){
+        switch (shapeNumber) {
+            case 1: return Color.yellow; // Oミノ
+            case 2: return Color.magenta; // Tミノ
+            case 3: return Color.green; // Sミノ
+            case 4: return Color.red; // Zミノ
+            case 5: return Color.orange; // Lミノ
+            case 6: return Color.blue; // Jミノ
+            case 7: return Color.cyan; // Iミノ
+            default: return Color.black; // 空
+        }
+    }
+
+    public GameManage getGameManage(){
+        return gm;
+    }
 }
